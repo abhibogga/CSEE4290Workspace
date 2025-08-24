05d01678d8fb457d6bbc5e393de2e1eedcd214af
abhi boggavarapu <abhibogga@gmail.com>
2025-08-24 16:35:03 -0400

    adding word doc so we have scommands to look at

--- labs/lab1/test_STRCPY.s
+++ labs/lab1/test_STRCPY.s
@@ -9,4 +9,13 @@ _start:
     STURB W5, [X0]
 	STURB W6, [X0, #1]
     STURB WZR, [X0, #2] 
+
+    LDURB  W2, [X0]  // Load byte into X2 from memory pointed to by X0 (*src)
+    ADD X0, X0, #1 // Increment src pointer
+    STURB  W2, [X1]  // Store byte in X2 into memory pointed to by X2 (*dst)
+    ADD  X1, X1, #1     // Increment dst pointer
+    CMP   X2, #0         // Was the byte 0? 
+    BNE   _strcpyloop    // If not, repeat the _strcpyloop
+    
+
     YIELD
