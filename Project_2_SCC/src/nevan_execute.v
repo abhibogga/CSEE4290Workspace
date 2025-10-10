8d33037f622639dc67898a41866250cc80f65ef4
Nevan Mukherjee <nm67037@uga.edu>
2025-10-10 01:25:07 -0400

    pushing execute move - nevan

---
+++ Project_2_SCC/src/nevan_execute.v
@@ -0,0 +1,23 @@
+module execute(dataRegisterImm, dest_reg, imm, dest_reg_value);
+
+	input reg dataRegisterImm;
+	input reg [15:0] imm;
+	output reg [3:0] dest_reg;
+	output reg [15:0] dest_reg_value;
+	
+
+	always @(*) begin
+		case (dataRegisterImm)
+			1'b0: begin
+			
+			end	//NO OP
+			1'b1: begin
+				dest_reg = output_destRegister; //pass this register 
+								//to register file
+				dest_reg_value = imm; //also pass this to register file
+			end
+			default: begin
+				//NO OP
+			end
+		endcase
+	end
