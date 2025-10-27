7546f231f7834db8443547a178ae7d1aaca53d87
Elijah Pham <etp51019@uga.edu>
2025-10-26 23:40:57 -0400

    Meaningful comments added to Kadane_test

--- Project_2_SCC/src/assembler/tests/Kadane_max_alg_test.asm
+++ Project_2_SCC/src/assembler/tests/Kadane_max_alg_test.asm
@@ -1,3 +1,4 @@
+;Comments provided both by Group 7 and CHATGPT to reinforce understanding
     ORG     #0x400
 
     FCB     #1,  #0, #0, #0
@@ -11,27 +12,29 @@
     FCB     #9,  #0, #0, #0
     FCB     #10, #0, #0, #0
 
+    ; ^^All FCB's formatted like that such that we can provide 4 bytes vs just 1
+
     ORG     #0x500
 RESULT:
-    FCB     #0, #0, #0, #0
+    FCB     #0, #0, #0, #0 ; Default value for result
 
-    ORG     #0
+    ORG     #0x000
 
 setup:
     CLR     R4
-    ADD     R4, R4, #0x400
+    ADD     R4, R4, #0x400      ; base address = 0x400
 
     CLR     R5
-    ADD     R5, R5, #10 //Counter
+    ADD     R5, R5, #10        ; length = 10 [Counter]
 
     CLR     R6
-    ADD     R6, R6, #0x500 //Result Address
+    ADD     R6, R6, #0x500     ; RESULT address = 0x500
 
 initialize:
     LOAD    R0, R4             ; first element x0
     ADD     R1, R0, #0         ; current_sum = x0
     ADD     R2, R0, #0         ; max_sum = x0
-    ADD     R4, R4, #4         ; advance ptr (word stride)
+    ADD     R4, R4, #4         ; advance address (word stride)
     SUB     R5, R5, #1         ; remaining count-- (now 9)
 
 kadane_loop:
@@ -39,10 +42,9 @@ kadane_loop:
     SUBS    R7, R5, #0
     B.eq    done
 
-    LOAD    R0, R4             ; x = *ptr
+    LOAD    R0, R4             ; x = *address
     ADDS    R3, R1, R0         ; temp = current_sum + x  (sets flags)
 
-    ; current_sum = max(temp, x) using signed compare
     SUBS    R7, R3, R0         ; temp - x  (sets N,Z,V)
     B.lt    use_x              ; if temp < x, use x
     ADD     R1, R3, #0         ; current_sum = temp
@@ -59,10 +61,9 @@ step:
     ADD     R4, R4, #4         ; next element
     SUB     R5, R5, #1         ; remaining count--
 
-    ; loop if remaining != 0 (set flags from R5 without faking them)
     ADDS    R7, R5, #0
     B.ne    kadane_loop
 
 done:
     STOR    R2, R6             ; RESULT = max_sum @ 0x500
-    HALT
+    HALT                       ; Final Result should be 0x00000037
