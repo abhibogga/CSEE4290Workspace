af6bb6766a8904589749d2d7bc21ca6a838b9d2b
abhi boggavarapu <abhibogga@gmail.com>
2025-10-28 12:27:26 -0400

    changed some compiler things up, for some reason it won't compile savf, don't know why tho

--- Project_2_SCC/src/run.ps1
+++ Project_2_SCC/src/run.ps1
@@ -1,4 +1,37 @@
-iverilog -o test shift_count_tb.v 
+param(
+    [string]$Profile = "",
+    [string]$AsmFile = "",
+    [string]$VerilogFile = ""
+)
+
+# --- PRESET GROUP DEFINITIONS ---
+switch ($Profile.ToLower()) {
+    "group2" {
+        $AsmFile = "simple_crc.asm"
+        $VerilogFile = "scc_tb_simple_crc.v"
+    }
+    "group6" {
+        $AsmFile = "assembler/tests/ALU_Test.asm"
+        $VerilogFile = "alu_tb.v"
+    }
+    "mem" {
+        $AsmFile = "assembler/tests/MemTest.asm"
+        $VerilogFile = "memory_tb.v"
+    }
+
+    "" {
+        Write-Error "❌ Invalid profile '$Profile'. Valid options are: group2, group6, mem."
+        exit 1
+    }
+    default {
+        Write-Error "❌ Invalid profile '$Profile'. Valid options are: group2, group6, mem."
+        exit 1
+    }
+}
+python3 assembler/parser/assembler.py assembler/tests/$AsmFile assembler/parser/instructions.json
+
+
+iverilog -o test $VerilogFile 
 vvp ./test
 gtkwave dump.vcd
 
