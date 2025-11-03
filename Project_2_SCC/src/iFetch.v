99bd56ee41e348139ea9ec67015cd04e1c640f9c
Eli Pham <108551106+TheAntiBreacher@users.noreply.github.com>
2025-11-02 19:30:44 -0500

    Update iFetch.v

--- Project_2_SCC/src/iFetch.v
+++ Project_2_SCC/src/iFetch.v
@@ -6,23 +6,35 @@ module iFetch(
     input [31:0] fetchedInstruction,
     input exeOverride, 
     input [15:0] exeData, //15 bit imm
+	input mul_trigger,
+    input mul_release,					
+					  
+ 
+					
+							   
+					   
+	input [31:0] readDataFirst,
+    input [6:0] opcode,				   
     output reg [31:0] programCounter,
     output reg [31:0] filteredInstruction
 );
 
     //Registers/States here: 
     reg [1:0] state, stateNext; 
-    parameter sIdle = 0, sFilter = 1;
+    parameter sIdle = 0, sFilter = 1, sUcode = 2;
 
-    reg [31:0] PC; 
+    //reg [31:0] PC; 
     reg [31:0] PC_next;  // <--- added
 
     //Branching offset things
     wire [15:0] imm16 = fetchedInstruction[15:0];
     wire signed [31:0] branchOffsetAddress = {{16{imm16[15]}}, imm16};
+									
 
     wire [15:0] imm16_exe = exeData;
     wire signed [31:0] branchOffsetAddress_exe = {{16{imm16_exe[15]}}, imm16_exe};
+
+	   
 
     //====================
     // Sequential Logic
@@ -31,14 +43,15 @@ module iFetch(
         state <= stateNext; 
 
         if (rst) begin 
-            PC <= 0; 
+            //PC <= 0; 
             programCounter <= 0; 
             state <= sIdle; 
         end 
+									  
         else begin 
-            if (state == sFilter) begin 
+            if (state != sIdle) begin 
                 // Update PC and programCounter together
-                PC <= PC_next;
+                //PC <= PC_next;
                 programCounter <= PC_next;
             end
         end
@@ -47,9 +60,10 @@ module iFetch(
     //====================
     // Combinational Logic
     //====================
+				  
     always @(*) begin 
         // default
-        PC_next = PC + 4;
+        PC_next = programCounter + 4;
 
         case (state)
             sIdle: begin 
@@ -57,7 +71,7 @@ module iFetch(
                     stateNext = sIdle; 
                 end else begin 
                     stateNext = sFilter; 
-                    PC_next = 0; 
+                    programCounter = 0; 
                 end
             end
 
@@ -67,25 +81,66 @@ module iFetch(
 
                 // === Conditional branch override from EXE ===
                 if (exeOverride) begin 
-                    PC_next = PC + branchOffsetAddress_exe;
+                    PC_next = programCounter + branchOffsetAddress_exe;
                 end 
+						   
+							
+						  
 
+							  
+
+
+												   
+																	   
                 // === Unconditional branch (B) ===
                 else if (fetchedInstruction[31:25] == 7'b1100000) begin
-                    PC_next = PC + branchOffsetAddress;
+                    PC_next = programCounter + branchOffsetAddress;
                 end 
 
                 // === NOP ===
                 else if (fetchedInstruction[31:25] == 7'b1100100) begin 
-                    PC_next = PC + 4; 
+                    PC_next = programCounter + 4; 
                 end 
 
+				else if (mul_trigger) begin
+					PC_next = programCounter + 4; 
+					stateNext = sUcode;
+				end			 
+						   
+												  
+				   
+			   
                 // === Default ===
                 else begin 
-                    PC_next = PC + 4; 
+                    PC_next = programCounter + 4; 
                 end
             end
 
+		   sUcode: begin
+				filteredInstruction = fetchedInstruction;
+				PC_next = programCounter; //tryna keep it frozen
+				if (mul_release) begin
+					stateNext = sFilter;
+				  
+				end
+				else begin
+					stateNext = sUcode;
+				end
+
+			end
+	 
+		 
+
+
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																											 
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																											 
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																											 
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																											 
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																											 
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																											 
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																											 
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																											 
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																											 
             default: 
                 stateNext = sIdle;
         endcase
